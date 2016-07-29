c45c80812984414a3133dee412a1015f9577acc3
Ethan Furman <ethan@stoneleaf.us>
2016-07-28 23:15:03 -0700

    use and modify (do not replace) output from parent call

--- controllers.py
+++ controllers.py
@@ -10,21 +10,14 @@ class SampleCrmRequest(controllers.SampleRequest):
     _cp_path = '/samplerequest'
 
     def get_sales_left(self, order):
-        if order.send_to == 'Client':
-            return super(SampleCrmRequest, self).get_sales_left(order)
-        main = [
-                ['Department', order.department],
-                ['Request by', order.user_id.name],
-                ['Created on', order.create_date],
-                ['Samples Must', order.target_date_type + ' by ' + order.target_date],
-                ['Send to', order.send_to],
-                ]
-        recip = [
-                ['Recipient',
-                    '\n'.join([
-                        t for t in
-                        (order.lead_id.name_get, order.lead_company, order.lead_name)
-                        if t
-                        ])],
-                ]
-        return main + recip
+        sales_left = super(SampleCrmRequest, self).get_sales_left(order)
+        if order.request_type == 'Customer':
+            return sales_left
+        for line in sales_left:
+            if line[0] == 'Recipient':
+                line[1] = '\n'.join([
+                            t for t in
+                            (order.lead_id.name_get, order.lead_company, order.lead_name)
+                            if t
+                            ])
+        return sales_left
